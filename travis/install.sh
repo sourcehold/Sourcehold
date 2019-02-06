2fda9070fc382a7742ffc71b9659df17565dc53d
metalvoidzz <metalvoidzz@gmail.com>
2019-02-06 20:51:51 +0100

    CMakeLists.txt fixes

--- travis/install.sh
+++ travis/install.sh
@@ -2,6 +2,7 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test -y
 sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots -y
 sudo add-apt-repository -y ppa:nschloe/boost-nightly
 sudo add-apt-repository -y ppa:mc3man/trusty-media
+sudo add-apt-repository -y ppa:jonathonf/ffmpeg-3
 
 sudo apt update -qq
 sudo apt install -y -qq libsdl2-dev 
