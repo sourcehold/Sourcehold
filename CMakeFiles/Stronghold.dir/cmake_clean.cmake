b31d54a4968ca603e5c2d7f6209807901b4ca967
metalvoidzz <metalvoidzz@gmail.com>
2019-02-06 21:15:53 +0100

    Stuff

---
+++ CMakeFiles/Stronghold.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stronghold.dir/src/Game.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/MainMenu.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Parsers/BinkVideo.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Parsers/Gm1File.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Parsers/MlbFile.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Parsers/Parser.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Parsers/TgxFile.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Parsers/VolumeTxt.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Rendering/Display.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Rendering/RenderList.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Rendering/Renderer.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Rendering/StaticElement.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Rendering/Texture.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/Sound/Sound.cpp.o"
+  "CMakeFiles/Stronghold.dir/src/System/Logger.cpp.o"
+  "Stronghold.pdb"
+  "Stronghold"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stronghold.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
