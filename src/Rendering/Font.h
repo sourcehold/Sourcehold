ff0f1c4e1679ac971c957b7c6ddd67105becee1f
FilipJanitor <filip.janitor@gmail.com>
2019-05-10 19:18:33 +0200

    Fix and add feature to text rendering

--- src/Rendering/Font.h
+++ src/Rendering/Font.h
@@ -20,7 +20,7 @@ namespace Sourcehold
 
         bool LoadFonts(std::shared_ptr<Game::GameManager> mgr);
         void UnloadFonts();
-        void RenderText(const std::wstring& text, int32_t x, int32_t y, double scaleFactor = 1, Font type = FONT_SMALL);
-        void RenderText(const std::wstring& text, double x, double y, double scaleFactor = 1, Font type = FONT_SMALL);
+        void RenderText(const std::wstring& text, int32_t x, int32_t y, double scaleFactor = 1, Font type = FONT_SMALL, bool illumination = false);
+        void RenderText(const std::wstring& text, double x, double y, double scaleFactor = 1, Font type = FONT_SMALL, bool illumination = false);
     }
 }
