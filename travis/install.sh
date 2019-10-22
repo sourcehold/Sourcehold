26ea424a058f0a012f8da28004198a25ec5f1a38
metalvoidzz <metalvoidzz@gmail.com>
2019-10-22 17:34:40 +0200

    Remove boost from install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -5,7 +5,6 @@ sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
 sudo add-apt-repository -y ppa:mc3man/trusty-media
 sudo add-apt-repository -y ppa:jonathonf/ffmpeg-3
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository -y ppa:nschloe/boost-nightly
 
 sudo apt update -qq
 sudo apt install -y -qq libsdl2-dev
@@ -14,6 +13,5 @@ sudo apt install -y -qq libavformat-dev
 sudo apt install -y -qq libswscale-dev
 sudo apt install -y -qq ffmpeg
 sudo apt install -y -qq libopenal-dev
-sudo apt install -y -qq libboost-all-dev
 sudo apt install -y -qq g++-6
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90
