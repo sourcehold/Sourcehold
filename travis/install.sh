d072f3a50d833eace7a0e3d375975c8700d5dddf
metalvoidzz <metalvoidzz@gmail.com>
2019-12-31 00:46:09 +0100

    Fix travis build (attempt #2)

--- travis/install.sh
+++ travis/install.sh
@@ -3,15 +3,15 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
 sudo add-apt-repository -y ppa:mc3man/trusty-media
-sudo add-apt-repository -y ppa:jonathonf/ffmpeg-3
+sudo add-apt-repository -y ppa:jonathonf/ffmpeg-4
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
 sudo apt update -qq
 sudo apt install -y -qq libsdl2-dev
+sudo apt install -y -qq ffmpeg
 sudo apt install -y -qq libavcodec-dev
 sudo apt install -y -qq libavformat-dev
 sudo apt install -y -qq libswscale-dev
-sudo apt install -y -qq ffmpeg
 sudo apt install -y -qq libopenal-dev
 sudo apt install -y -qq g++-7
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
