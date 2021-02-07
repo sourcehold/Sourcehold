9e51cdad82ebb549fcf0d462e0c326c13d76c7b7
natewind <sdksing@gmail.com>
2021-02-07 12:27:28 +0300

    Range-based for loop

--- src/Rendering/TextureAtlas.cpp
+++ src/Rendering/TextureAtlas.cpp
@@ -47,9 +47,8 @@ void TextureAtlas::Allocate(std::vector<std::pair<uint32_t, uint32_t>>& entries)
 
     uint32_t width = 0, height = 0;
     uint32_t px = 0, py = 0;
-    for(size_t i = 0; i < sorted.size(); i++) {
-        Elem e = sorted[i];
 
+    for(auto const &e : sorted) {
         if(px > MAX_X_RESOLUTION) {
             px = 0;
             py = height;
