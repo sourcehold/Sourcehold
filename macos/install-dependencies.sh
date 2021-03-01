030e6299706812112cab930e189205efaa84c133
Daniel Mizerski <duelsik@gmail.com>
2021-03-01 18:51:13 +0100

    Install EnTT on macos via compiling

--- macos/install-dependencies.sh
+++ macos/install-dependencies.sh
@@ -7,4 +7,15 @@ echo "Installing FFmpeg..."
 brew install ffmpeg
 
 echo "Installing skypjacks EnTT..."
-brew install skypjack/entt/entt
+# brew install skypjack/entt/entt
+
+mkdir /deps
+cd /deps
+
+git clone --depth 1 https://github.com/skypjack/entt
+cd entt
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
