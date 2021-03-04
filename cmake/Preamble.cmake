20f7c3a742bc9429de30ee5d9505d8cb0de944c3
Fabian Seidl <fabianseidl@fseidl.eu>
2021-03-04 20:09:05 +0100

    add macos subdirectory for macos libs moved several commands to preamble.cmake

---
+++ cmake/Preamble.cmake
@@ -0,0 +1,8 @@
+# PREAMBLE
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+include(cmake/AddImportedTarget.cmake)
+include(cmake/Dependencies.cmake)
+
+
