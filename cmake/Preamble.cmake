28ce7b12f504354d2fdb95d6492c34e756cf1370
Fabian Seidl <fabianseidl@fseidl.eu>
2021-03-08 15:41:08 +0100

    add PreventInSourceBuild.cmake

--- cmake/Preamble.cmake
+++ cmake/Preamble.cmake
@@ -2,6 +2,7 @@
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
+include(cmake/PreventInSourceBuild.cmake)
 include(cmake/PlatformCheck.cmake)
 include(cmake/AddImportedTarget.cmake)
 include(cmake/Dependencies.cmake)
