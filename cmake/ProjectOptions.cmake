43dcbf9527510de603099b187a295f5e0e4a1e8c
Fabian Seidl <fabianseidl@fseidl.eu>
2021-03-08 15:57:54 +0100

    add stricter warnings to ProjectOptions

--- cmake/ProjectOptions.cmake
+++ cmake/ProjectOptions.cmake
@@ -1,22 +1,5 @@
+include(cmake/Warnings.cmake)
+
 add_library(ProjectOptions INTERFACE)
 target_compile_features(ProjectOptions INTERFACE cxx_std_17)
-
-if(MSVC)
-# TODO(seidl):
-# move this somewhere else
-  target_compile_options(ProjectOptions INTERFACE -D_CRT_SECURE_NO_WARNINGS)
-  foreach( OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES} )
-    string( TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG )
-    set( CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_BINARY_DIR} )
-    set( CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_BINARY_DIR} )
-    set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_BINARY_DIR} )
-  endforeach( OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES )
-else()
-  target_compile_options(ProjectOptions INTERFACE
-    -Wno-reorder
-    -pedantic-errors
-    -Ofast
-    -fno-fast-math)
-endif()
-# TODO(seidl):
-#add stricter warnings and linting
+set_project_warnings(ProjectOptions)
