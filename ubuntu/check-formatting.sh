e436b48321e118a890699398941bc8b94deb05c2
Fabian Seidl <fabianseidl@fseidl.eu>
2021-03-13 22:47:51 +0100

    specify file as formatting style

--- ubuntu/check-formatting.sh
+++ ubuntu/check-formatting.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 FILES=$(find src -regex ".*\.\(hpp\|cpp\|cc\|c\|h\)$")
 
-if clang-format $FILES --output-replacements-xml | grep "<replacement " > replacements.xml ;then
+if clang-format $FILES -style=file --output-replacements-xml | grep "<replacement " > replacements.xml ;then
   echo Some files require formatting
   echo Be sure to use this version of clang-format:
   clang-format --version
