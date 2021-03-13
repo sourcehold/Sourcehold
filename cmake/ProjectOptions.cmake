e1a2bca523db787ec9283b80c435831aa5d55fb9
Fabian Seidl <fabianseidl@fseidl.eu>
2021-03-13 18:43:28 +0100

    add static analyzers add sanitzers

--- cmake/ProjectOptions.cmake
+++ cmake/ProjectOptions.cmake
@@ -1,5 +1,9 @@
 include(cmake/Warnings.cmake)
+include(cmake/StaticAnalyzers.cmake)
+include(cmake/Sanitizers.cmake)
 
 add_library(ProjectOptions INTERFACE)
 target_compile_features(ProjectOptions INTERFACE cxx_std_17)
+
 set_project_warnings(ProjectOptions)
+enable_sanitizers(ProjectOptions)
