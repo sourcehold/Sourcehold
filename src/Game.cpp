e4d4bb01ac045690caf47788dde382262f1e4d7e
Fabian Seidl <fabianseidl@fseidl.eu>
2021-03-14 13:28:57 +0100

    format code base

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,28 +3,20 @@
 #include <string>
 #include <vector>
 #include <regex>
-
 #include "System/Commandline.h"
-
 #include "World.h"
 #include "GameManager.h"
 #include "Startup.h"
-
 #include "Parsers/MlbFile.h"
-
 #include "System/System.h"
 #include "System/Logger.h"
 #include "System/FileUtil.h"
-
 #include "ECS/Manager.h"
-
 #include "GUI/NarrScreen.h"
 #include "GUI/MainMenu.h"
-
 #if SOURCEHOLD_IOS
 #include "Common/SHPathUtils.h"
 #endif
-
 using namespace Sourcehold;
 using namespace Game;
 using namespace Audio;
@@ -33,14 +25,12 @@ using namespace Assets;
 using namespace Parsers;
 using namespace Rendering;
 using namespace GUI;
-
 int MainLoop(UIState state) {
   switch (state) {
     case MAIN_MENU: {
       MainMenu *menu = new MainMenu();
       state = menu->EnterMenu();
       delete menu;
-
       MainLoop(state);
     } break;
     case EXIT_GAME:
@@ -48,53 +38,40 @@ int MainLoop(UIState state) {
     default:
       break;
   }
-
   ExitGame();
   return EXIT_SUCCESS;
 }
-
 int EnterLoadingScreen() {
   std::shared_ptr<TgxFile> tgx_loading = GetTgx("gfx/frontend_loading.tgx");
-
   /* Get the assets */
   std::vector<ghc::filesystem::path> files =
       GetDirectoryRecursive(GetDirectory(), ".ani");
   if (files.empty()) {
     return EXIT_FAILURE;
   }
-
   /* Preload some assets */
   uint32_t index = 0;
-
   /* Calculate the position */
   int px = (GetWidth() / 2) - (1024 / 2);
   int py = (GetHeight() / 2) - (768 / 2);
-
   ResetTarget();
-
   Resolution res = GetResolution();
   StrongholdEdition ed = GetEdition();
-
   while (Running() && index < files.size() - 1) {
     ClearDisplay();
-
 #if RENDER_LOADING_BORDER == 1
     if (ed == STRONGHOLD_HD && res != RESOLUTION_800x600) {
       RenderMenuBorder();
     }
 #endif
-
     /* Load a file */
     ghc::filesystem::path path = files.at(index);
     Cache(path);
     index++;
-
     /* Normalized loading progess */
     double progress = (double)index / (double)files.size();
-
     /* Render the background */
     Render(*tgx_loading, px, py);
-
     /* Render the loading bar */
     RenderRect(
         Rect<int>{px + (1024 / 2) - (450 / 2), py + int(768.0 / 1.3), 450, 35},
@@ -105,23 +82,17 @@ int EnterLoadingScreen() {
     RenderRect(Rect<int>{px + 5 + (1024 / 2) - (450 / 2),
                          py + 5 + int(768.0 / 1.3), int(440.0 * progress), 25},
                0, 0, 0, 255, true);
-
     FlushDisplay();
-
     SDL_Delay(1);
   }
-
   // TODO - Maybe add next loading bar?
   ECS::Manager::GetInstance();
-
   return Running() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
 static const int resolutions[][2] = {{800, 600},   {1024, 768},  {1280, 720},
                                      {1280, 1024}, {1366, 768},  {1440, 900},
                                      {1600, 900},  {1600, 1200}, {1680, 1050},
                                      {1920, 1080}, {-1, -1}};
-
 int StartGame(GameOptions &opt) {
   // Init logger //
 #if SOURCEHOLD_UNIX == 1 && !defined(SOURCEHOLD_ANDROID)
@@ -129,23 +100,19 @@ int StartGame(GameOptions &opt) {
 #else
   Logger::SetColorOutput(false);
 #endif
-
   if (opt.color >= 0) Logger::SetColorOutput(opt.color == 1);
-
   Resolution res;
   // Convert resolution //
   bool found = false;
   for (int i = 0; i < (sizeof(resolutions) / sizeof(int)) / 2; i++) {
     int w = resolutions[i][0];
     int h = resolutions[i][1];
-
     if (w == opt.width && h == opt.height) {
       res = static_cast<Resolution>(i);
       found = true;
       break;
     }
   }
-
   if (!found) {
     /**
      * If the selected resolution is not supported by the original game,
@@ -156,12 +123,10 @@ int StartGame(GameOptions &opt) {
                              "lead to ugly scaling!"
                           << std::endl;
   }
-
   // Init game //
   Logger::message(GAME) << "Starting version " SOURCEHOLD_VERSION_STRING
                            " (" SOURCEHOLD_BUILD ")"
                         << std::endl;
-
   if (!InitManager(opt, res) || !LoadGameData()) {
     ErrorMessageBox("Something went wrong...",
                     std::string("Please make sure the data directory contains "
@@ -169,41 +134,31 @@ int StartGame(GameOptions &opt) {
                         +"Current path: " + GetDirectory().string());
     return EXIT_FAILURE;
   }
-
   Logger::message(GAME) << "Done" << std::endl;
-
   Startup *start = new Startup();
-
   int ret = EnterLoadingScreen();
   if (ret != EXIT_SUCCESS) return ret;
-
   UIState state = MAIN_MENU;
   if (!opt.skip) {
     start->PlayMusic();
     state = start->Begin();
   }
-
   delete start;
   return MainLoop(state);
 }
-
 // Do not #undef main on iOS, because in other case it will be not possible to
 // build project
 #ifndef SOURCEHOLD_IOS
 #undef main
 #endif  // SOURCEHOLD_IOS
-
 /* Common entry point across all platforms */
 int main(int argc, char **argv) {
   namespace po = cxxopts;
   namespace fs = ghc::filesystem;
-
   // Parse commandline //
   try {
     GameOptions opt;
-
     po::Options config("Sourcehold", "Open-source Stronghold");
-
     config.add_options()("h,help", "Print this info")("v,version",
                                                       "Print version string")(
         "resolutions", "List available resolutions and exit")(
@@ -223,38 +178,31 @@ int main(int argc, char **argv) {
         "nosound", "Disable sound entirely", po::value<bool>(opt.nosound))(
         "nothread", "Disable threading", po::value<bool>(opt.nothread))(
         "nocache", "Disable asset caching", po::value<bool>(opt.nocache));
-
     auto result = config.parse(argc, argv);
     if (result["help"].as<bool>()) {
       std::cout << config.help() << std::endl;
       return EXIT_SUCCESS;
     }
-
     if (result["resolutions"].as<bool>()) {
       for (int i = 0; i < (sizeof(resolutions) / sizeof(int)) / 2; i++) {
         std::cout << resolutions[i][0] << "x" << resolutions[i][1] << std::endl;
       }
       return EXIT_SUCCESS;
     }
-
     if (result.count("color") > 0)
       opt.color = result["color"].as<bool>();
     else
       opt.color = -1;
-
     opt.ndisp = result["disp"].as<uint16_t>();
 #ifdef SOURCEHOLD_IOS
     char resourcesPath[512];
-
     SHQueryMainBundleResourcesDirectoryPath(resourcesPath, 512);
     opt.dataDir = std::string(resourcesPath).append("/data");
 #else
     opt.dataDir = result["path"].as<std::string>();
 #endif  // SOURCEHOLD_IOS
-
     std::regex regex("(\\d+)x(\\d+)");
     std::smatch match;
-
 #if defined(SOURCEHOLD_ANDROID) || defined(SOURCEHOLD_IOS)
     // Width and height arguments will be ignored on Android and iOS //
     opt.width = opt.height = -1;
@@ -269,26 +217,23 @@ int main(int argc, char **argv) {
       opt.height = 600;
     }
 #endif  // defined(SOURCEHOLD_ANDROID) || defined(SOURCEHOLD_IOS)
-
     return StartGame(opt);
   } catch (po::OptionException &e) {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
   }
 }
-
 #if SOURCEHOLD_MINGW == 1 && 0
-
 #include <windows.h>
 #include <string>
 #include <vector>
-
 /* Windows specific entry point */
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevIns, LPSTR lpszArgument,
                    int iShow) {
   /* Convert argument list */
   int w_argc = 0;
   LPWSTR *w_argv = CommandLineToArgvW(GetCommandLineW(), &w_argc);
+
   if (w_argv) {
     std::vector<std::string> argv_buf;
     argv_buf.reserve(w_argc);
@@ -305,20 +250,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevIns, LPSTR lpszArgument,
 
     std::vector<char *> argv;
     argv.reserve(argv_buf.size());
+
     for (std::vector<std::string>::iterator i = argv_buf.begin();
-         i != argv_buf.end(); ++i)
+         i != argv_buf.end(); ++i) {
       argv.push_back((char *)i->c_str());
-
+    }
     int code = main(argv.size(), &argv[0]);
-
     LocalFree(w_argv);
+
     return code;
   }
-
   int code = main(0, NULL);
-
   LocalFree(w_argv);
   return code;
 }
-
 #endif
--- src/Rendering/Display.cpp
+++ src/Rendering/Display.cpp
@@ -2,14 +2,11 @@
 #include "Rendering/Display.h"
 #include "System/Logger.h"
 #include "System/Config.h"
-
 using namespace Sourcehold;
 using namespace System;
-
 SDL_Window *_window;
 bool _fullscreen, _nograb;
 int _width, _height;
-
 static int ResizeEventWatcher(void *data, SDL_Event *event) {
   if (event->type == SDL_WINDOWEVENT &&
       event->window.event == SDL_WINDOWEVENT_RESIZED) {
@@ -18,10 +15,8 @@ static int ResizeEventWatcher(void *data, SDL_Event *event) {
       SDL_GetWindowSize(win, &_width, &_height);
     }
   }
-
   return 0;
 }
-
 bool Rendering::InitDisplay(const std::string &title, int width, int height,
                             int index, bool fullscreen, bool noborder,
                             bool nograb, bool resize) {
@@ -29,40 +24,31 @@ bool Rendering::InitDisplay(const std::string &title, int width, int height,
     Logger::error(RENDERING) << SDL_GetError() << std::endl;
     return false;
   }
-
   int param = SDL_WINDOW_SHOWN | SDL_WINDOW_INPUT_FOCUS;
-
   _fullscreen = fullscreen;
   _nograb = nograb;
-
   if (fullscreen) {
     param |= SDL_WINDOW_FULLSCREEN;
   }
-
   if (noborder) {
     param |= SDL_WINDOW_BORDERLESS;
   }
-
   if (resize) {
     param |= SDL_WINDOW_RESIZABLE;
   }
-
   // Select display //
   int displays = SDL_GetNumVideoDisplays();
   if (index >= displays) index = 0;
-
   auto displayBounds = std::vector<SDL_Rect>(displays);
   for (int i = 0; i < displays; i++) {
     displayBounds[i] = {0, 0, 0, 0};
     SDL_GetDisplayBounds(i, &displayBounds[i]);
   }
-
 #if defined(SOURCEHOLD_ANDROID) || defined(SOURCEHOLD_IOS)
   // On Android and iOS, only use the native resolution //
   if (width < 0 || height < 0) {
     SDL_DisplayMode mode;
     SDL_GetCurrentDisplayMode(index, &mode);
-
     width = mode.w;
     height = mode.h;
     // Flip width and height to force landscape orientation //
@@ -71,7 +57,6 @@ bool Rendering::InitDisplay(const std::string &title, int width, int height,
     }
   }
 #endif  // defined(SOURCEHOLD_ANDROID) || defined(SOURCEHOLD_IOS)
-
   _window = SDL_CreateWindow(
       title.c_str(),
       displayBounds[index].x + (displayBounds[index].w / 2) - width / 2,
@@ -81,23 +66,18 @@ bool Rendering::InitDisplay(const std::string &title, int width, int height,
     Logger::error(RENDERING)
         << "Unable to create SDL2 window: " << SDL_GetError() << std::endl;
   }
-
   if (!nograb) {
     SDL_SetWindowGrab(_window, SDL_TRUE);
   }
-
   _width = width;
   _height = height;
   SDL_AddEventWatch(ResizeEventWatcher, nullptr);
-
   return true;
 }
-
 void Rendering::DestroyDisplay() {
   SDL_DestroyWindow(_window);
   SDL_Quit();
 }
-
 void Rendering::ToggleFullscreen() {
   int err =
       SDL_SetWindowFullscreen(_window, _fullscreen ? 0 : SDL_WINDOW_FULLSCREEN);
@@ -107,45 +87,34 @@ void Rendering::ToggleFullscreen() {
   }
   _fullscreen = !_fullscreen;
 }
-
 void Rendering::GrabMouse() {
   if (!_nograb && SDL_GetWindowGrab(_window) == SDL_FALSE) {
     SDL_SetWindowGrab(_window, SDL_TRUE);
   }
 }
-
 void Rendering::ReleaseMouse() {
   if (SDL_GetWindowGrab(_window) == SDL_TRUE) {
     SDL_SetWindowGrab(_window, SDL_FALSE);
   }
 }
-
 bool Rendering::IsDisplayOpen() { return true; }
-
 void Rendering::ErrorMessageBox(const std::string &title,
                                 const std::string &msg) {
   SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title.c_str(), msg.c_str(),
                            _window);
 }
-
 int Rendering::GetWidth() { return _width; }
-
 int Rendering::GetHeight() { return _height; }
-
 int Rendering::GetMouseX() {
   int p;
   SDL_GetMouseState(&p, NULL);
   return p;
 }
-
 int Rendering::GetMouseY() {
   int p;
   SDL_GetMouseState(NULL, &p);
   return p;
 }
-
 void Rendering::MouseOn() { SDL_ShowCursor(SDL_ENABLE); }
-
 void Rendering::MouseOff() { SDL_ShowCursor(SDL_DISABLE); }
-
 SDL_Window *Rendering::GetWindow() { return _window; }
