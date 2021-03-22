8808c34cbc773b67b861645eec47a4784b8c46d7
apodrugin <apodrugin@readdle.com>
2021-03-22 13:28:32 +0200

    Use custom template Info.plist file to force cmake to set up all necessary values. Add project resources using cmake to avoid manual resources management and let Xcode do all the work. Fix small issue in build script. (#67)

--- apple/cmake/MakeiOSAppBundleAndLinkFrameworks.cmake
+++ apple/cmake/MakeiOSAppBundleAndLinkFrameworks.cmake
@@ -1,63 +1,46 @@
 set(DATA_DIR_PATH ${CMAKE_SOURCE_DIR}/data)
 set(BUNDLE_DIR_PATH $<TARGET_FILE_DIR:${PROJECT_NAME}>)
 set(RESOURCES_DIR_PATH ${CMAKE_SOURCE_DIR}/apple/iOS/Resources)
+set(TEMPLATE_INFO_PLIST_PATH "${CMAKE_SOURCE_DIR}/apple/iOS/Supporting Files/Info.plist")
 set(INFO_PLIST_BUNDLE_DIR_PATH ${BUNDLE_DIR_PATH}/Info.plist)
 set(ACTOOL_GENERATED_PLIST_PATH ${CMAKE_BINARY_DIR}/assetcatalog_generated_info.plist)
 set(BUNDLE_IDENTIFIER "com.sourcehold.\${PRODUCT_NAME:identifier}")
   
+set(APP_RESOURCES
+  ${RESOURCES_DIR_PATH}/Base.lproj/LaunchScreen.storyboard
+  ${RESOURCES_DIR_PATH}/Assets.xcassets
+)
+ 
+if(EXISTS ${DATA_DIR_PATH})
+  set(APP_RESOURCES ${APP_RESOURCES} ${DATA_DIR_PATH})
+  source_group("Resources" FILES ${DATA_DIR_PATH})
+else()
+  message(WARNING "'data' directroy does not exist. Skipping adding to resources.")
+endif() # If data dir path exists
+ 
+target_sources(${PROJECT_NAME}
+  PRIVATE ${APP_RESOURCES}
+)
+  
 set_target_properties(${PROJECT_NAME} PROPERTIES
   MACOSX_BUNDLE TRUE
-  MACOSX_BUNDLE_GUI_IDENTIFIER ${BUNDLE_IDENTIFIER}
-  MACOSX_BUNDLE_SHORT_VERSION_STRING ${PROJECT_VERSION}
-  MACOSX_BUNDLE_BUNDLE_VERSION ${PROJECT_VERSION}
-  XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${BUNDLE_IDENTIFIER}
+  MACOSX_BUNDLE_INFO_PLIST ${TEMPLATE_INFO_PLIST_PATH}
+  XCODE_ATTRIBUTE_PRODUCT_NAME ${PROJECT_NAME}
   XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH NO
-)
-  
-if(EXISTS ${DATA_DIR_PATH})
-  add_custom_command(TARGET ${PROJECT_NAME} PRE_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy_directory ${DATA_DIR_PATH} ${BUNDLE_DIR_PATH}/data
-  )
-else()
-  message("'data' directroy does not exist. Skipping copy to app bundle.")
-endif() # If data dir path exists
-  
-# All storyboards in the Base.lproj directory
-set(STORYBOARDS ${RESOURCES_DIR_PATH}/Base.lproj/*.storyboard)
-
-# The output folder for compiled storyboards
-set(STORYBOARD_OUT_DIR_PATH ${BUNDLE_DIR_PATH}/Base.lproj)
-
-# Compile all storyboards and add them to app bundle
-add_custom_command(TARGET ${PROJECT_NAME} PRE_BUILD
-  COMMAND mkdir -p ${STORYBOARD_OUT_DIR_PATH}
-  COMMAND for storyboard_path in ${STORYBOARDS}\; do ibtool $storyboard_path --compilation-directory ${STORYBOARD_OUT_DIR_PATH}\; done
-)
-
-# Compile main assets catalog and app icon
-add_custom_command(TARGET ${PROJECT_NAME} PRE_BUILD
-  COMMAND xcrun actool Assets.xcassets --compile ${BUNDLE_DIR_PATH} --compress-pngs --app-icon AppIcon --output-partial-info-plist ${ACTOOL_GENERATED_PLIST_PATH} --platform iphoneos --minimum-deployment-target ${CMAKE_OSX_DEPLOYMENT_TARGET}
-  WORKING_DIRECTORY ${RESOURCES_DIR_PATH}
-)
-  
-# Update app Info.plist with necessary values
-add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+  XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${BUNDLE_IDENTIFIER}
   # Specify that app is universal and can be run on iPhone (1) and iPad (2). By default cmake set this value to 1
   # and thus we get scaled iPhone app on iPad.
-  COMMAND plutil -replace UIDeviceFamily -json '[1, 2]' ${INFO_PLIST_BUNDLE_DIR_PATH}
-  # We do not want to participate in Slide Over and Split View, thus we have to set this flag to YES.
-  COMMAND plutil -replace UIRequiresFullScreen -bool YES ${INFO_PLIST_BUNDLE_DIR_PATH}
-  COMMAND plutil -replace UILaunchStoryboardName -string LaunchScreen ${INFO_PLIST_BUNDLE_DIR_PATH}
-  # Set arm64 device capability in order to filter ancient devices and avoid 32-bit architectures support.
-  COMMAND plutil -replace UIRequiredDeviceCapabilities -json '[\"arm64\"]' ${INFO_PLIST_BUNDLE_DIR_PATH}
-  # This values are necessary to show app icon
-  COMMAND plutil -replace CFBundleIcons -json `plutil -extract CFBundleIcons json -o - -- ${ACTOOL_GENERATED_PLIST_PATH}` ${INFO_PLIST_BUNDLE_DIR_PATH}
-  COMMAND plutil -replace CFBundleIcons~ipad -json `plutil -extract CFBundleIcons~ipad json -o - -- ${ACTOOL_GENERATED_PLIST_PATH}` ${INFO_PLIST_BUNDLE_DIR_PATH}
+  XCODE_ATTRIBUTE_TARGETED_DEVICE_FAMILY "1,2"
+  XCODE_ATTRIBUTE_ASSETCATALOG_COMPILER_APPICON_NAME AppIcon
+  RESOURCE "${APP_RESOURCES}"
 )
   
-# Remove unnecessary temporary files
+# Update app Info.plist with project name and version which are set using cmake
 add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
-  COMMAND rm ${ACTOOL_GENERATED_PLIST_PATH}
+  COMMAND plutil -replace CFBundleExecutable -string ${PROJECT_NAME} ${INFO_PLIST_BUNDLE_DIR_PATH}
+  COMMAND plutil -replace CFBundleIdentifier -string ${BUNDLE_IDENTIFIER} ${INFO_PLIST_BUNDLE_DIR_PATH}
+  COMMAND plutil -replace CFBundleLongVersionString -string ${PROJECT_VERSION} ${INFO_PLIST_BUNDLE_DIR_PATH}
+  COMMAND plutil -replace CFBundleShortVersionString -string ${PROJECT_VERSION} ${INFO_PLIST_BUNDLE_DIR_PATH}
 )
   
 target_link_libraries(${PROJECT_NAME}
