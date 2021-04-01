35c1cc71bbfb789c3ca9b70c46054212e6c8cd9b
Fabian Seidl <fabianseidl@fseidl.eu>
2021-04-01 11:37:43 +0200

    add arithmetic operators to Vector2

--- src/Rendering/Shapes.h
+++ src/Rendering/Shapes.h
@@ -1,20 +1,169 @@
 #pragma once
+#include <array>
+#include <functional>
 
 namespace Sourcehold {
 namespace Rendering {
-template <class T>
+template <typename T>
 struct Rect {
-  T x, y, w, h;
+  T x;
+  T y;
+  T w;
+  T h;
 };
 
-template <class T>
+template <typename T>
 struct Line {
-  T x1, y1, x2, y2;
+  T x1;
+  T y1;
+  T x2;
+  T y2;
 };
 
-template <class T>
+template <typename T>
 struct Vector2 {
-  T x, y;
+  T x;
+  T y;
+
+  template <typename F>
+  friend Vector2<F>& operator+=(Vector2<F>& l, const Vector2<F>& r);
+  template <typename F>
+  friend Vector2<F>& operator-=(Vector2<F>& l, const Vector2<F>& r);
+  template <typename F>
+  friend Vector2<F>& operator*=(Vector2<F>& l, const Vector2<F>& r);
+  template <typename F>
+  friend Vector2<F>& operator/=(Vector2<F>& l, const Vector2<F>& r);
+  template <typename F>
+  friend Vector2<F>& operator+=(Vector2<F>& l, const F& r);
+  template <typename F>
+  friend Vector2<F>& operator-=(Vector2<F>& l, const F& r);
+  template <typename F>
+  friend Vector2<F>& operator*=(Vector2<F>& l, const F& r);
+  template <typename F>
+  friend Vector2<F>& operator/=(Vector2<F>& l, const F& r);
+
+  template <typename F>
+  friend Vector2<F> operator+(const Vector2<F>& l, const Vector2<F>& r);
+  template <typename F>
+  friend Vector2<F> operator-(const Vector2<F>& l, const Vector2<F>& r);
+  template <typename F>
+  friend Vector2<F> operator*(const Vector2<F>& l, const Vector2<F>& r);
+  template <typename F>
+  friend Vector2<F> operator/(const Vector2<F>& l, const Vector2<F>& r);
+  template <typename F>
+  friend Vector2<F> operator+(const Vector2<F>& l, const F& r);
+  template <typename F>
+  friend Vector2<F> operator-(const Vector2<F>& l, const F& r);
+  template <typename F>
+  friend Vector2<F> operator*(const Vector2<F>& l, const F& r);
+  template <typename F>
+  friend Vector2<F> operator/(const Vector2<F>& l, const F& r);
+
+  template <typename OT>
+  operator Vector2<OT>() {
+    return Vector2<OT>{static_cast<OT>(x), static_cast<OT>(y)};
+  }
 };
+
+namespace Details {
+template <typename T, typename F>
+Vector2<T>& calc(Vector2<T>& l, const Vector2<T>& r, F f) {
+  l.x = f(l.x, r.x);
+  l.y = f(l.y, r.y);
+  return l;
+}
+
+template <typename T, typename F>
+Vector2<T>& calc(Vector2<T>& l, const T& r, F f) {
+  l.x = f(l.x, r);
+  l.y = f(l.y, r);
+  return l;
+}
+
+}  // namespace Details
+
+template <typename F>
+Vector2<F>& operator+=(Vector2<F>& l, const Vector2<F>& r) {
+  return Details::calc(l, r, std::plus());
+}
+template <typename F>
+Vector2<F>& operator-=(Vector2<F>& l, const Vector2<F>& r) {
+  return Details::calc(l, r, std::minus());
+}
+
+template <typename F>
+Vector2<F>& operator*=(Vector2<F>& l, const Vector2<F>& r) {
+  return Details::calc(l, r, std::multiplies());
+}
+
+template <typename F>
+Vector2<F>& operator/=(Vector2<F>& l, const Vector2<F>& r) {
+  return Details::calc(l, r, std::divides());
+}
+template <typename F>
+Vector2<F>& operator+=(Vector2<F>& l, const F& r) {
+  return Details::calc(l, r, std::plus());
+}
+template <typename F>
+Vector2<F>& operator-=(Vector2<F>& l, const F& r) {
+  return Details::calc(l, r, std::minus());
+}
+
+template <typename F>
+Vector2<F>& operator*=(Vector2<F>& l, const F& r) {
+  return Details::calc(l, r, std::multiplies());
+}
+
+template <typename F>
+Vector2<F>& operator/(Vector2<F>& l, const F& r) {
+  return Details::calc(l, r, std::divides());
+}
+
+template <typename F>
+Vector2<F> operator+(const Vector2<F>& l, const Vector2<F>& r) {
+  auto result = l;
+  return result += r;
+}
+template <typename F>
+Vector2<F> operator-(const Vector2<F>& l, const Vector2<F>& r) {
+  auto result = l;
+  return result -= r;
+}
+
+template <typename F>
+Vector2<F> operator*(const Vector2<F>& l, const Vector2<F>& r) {
+  auto result = l;
+  return result *= r;
+}
+
+template <typename F>
+Vector2<F> operator/(const Vector2<F>& l, const Vector2<F>& r) {
+  auto result = l;
+  return result /= r;
+}
+
+template <typename F>
+Vector2<F> operator+(const Vector2<F>& l, const F& r) {
+  auto result = l;
+  return result += r;
+}
+template <typename F>
+Vector2<F> operator-(const Vector2<F>& l, const F& r) {
+  auto result = l;
+  return result -= r;
+}
+
+template <typename F>
+Vector2<F> operator*(const Vector2<F>& l, const F& r) {
+  auto result = l;
+  return result *= r;
+}
+
+template <typename F>
+Vector2<F> operator/(const Vector2<F>& l, const F& r) {
+  auto result = l;
+  return result /= r;
+}
+
 }  // namespace Rendering
 }  // namespace Sourcehold
