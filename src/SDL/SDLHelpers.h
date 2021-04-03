d5e6244df995bdd1ea390577c45861fcb95cad52
Fabian Seidl <fabianseidl@fseidl.eu>
2021-04-03 14:01:18 +0200

    add more sdl helpers

--- src/SDL/SDLHelpers.h
+++ src/SDL/SDLHelpers.h
@@ -2,6 +2,7 @@
 
 #include <SDL.h>
 #include <memory>
+#include "Rendering/Shapes.h"
 
 namespace Sourcehold {
 namespace SDL {
@@ -20,6 +21,10 @@ struct SDL_Deleter {
     SDL_DestroyWindow(window);
   }
 };
+
+struct ColorMask {
+  uint32_t r, g, b, a;
+};
 }  // namespace Details
 
 using SDL_Surface_UQ = std::unique_ptr<SDL_Surface, Details::SDL_Deleter>;
@@ -32,22 +37,32 @@ using SDL_Texture_SH = std::shared_ptr<SDL_Texture>;
 using SDL_Renderer_SH = std::shared_ptr<SDL_Renderer>;
 using SDL_Window_SH = std::shared_ptr<SDL_Window>;
 
-struct ColorMask {
-  uint32_t r, g, b, a;
-};
-
 constexpr int SurfaceDepth = 32;
-constexpr ColorMask SurfaceMask{0xFF000000,  //
-                                0x00FF0000,  //
-                                0x0000FF00,  //
-                                0x000000FF};
+constexpr Details::ColorMask SurfaceMask{0xFF000000,  //
+                                         0x00FF0000,  //
+                                         0x0000FF00,  //
+                                         0x000000FF};
 constexpr auto BlendMode = SDL_BLENDMODE_BLEND;
 
+constexpr auto PixelFormat = SDL_PIXELFORMAT_RGBA4444;
+
 template <typename T>
-[[nodiscard]] T& At(SDL_Surface* surf, int x, int y) {
+[[nodiscard]] T& At(SDL_Surface* surf, Rendering::Vector2<int> pos) {
   static_assert(std::is_arithmetic_v<T>);
-  T* ptr = static_cast<T*>(surf->pixels)[y * surf->w + x];
-  return *ptr;
+  T* ptr = static_cast<T*>(surf->pixels);
+  return ptr[pos.y * surf->w + pos.x];
+}
+
+[[nodiscard]] inline SDL_Rect* ToSDLRectPtr(Rendering::Rect<int>& rect) {
+  using namespace Rendering;
+  static_assert(std::is_same_v<decltype(Rect<int>::x), decltype(SDL_Rect::x)>);
+  static_assert(std::is_same_v<decltype(Rect<int>::y), decltype(SDL_Rect::y)>);
+  static_assert(std::is_same_v<decltype(Rect<int>::w), decltype(SDL_Rect::w)>);
+  static_assert(std::is_same_v<decltype(Rect<int>::h), decltype(SDL_Rect::h)>);
+  static_assert(sizeof(Rect<int>) == sizeof(SDL_Rect));
+  static_assert(sizeof(Rect<int>*) == sizeof(SDL_Rect*));
+
+  return reinterpret_cast<SDL_Rect*>(&rect);
 }
 
 }  // namespace SDL
