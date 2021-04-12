34df30e4452ebf01befac42610c11361882eb345
Fabian Seidl <fabianseidl@fseidl.eu>
2021-04-12 14:45:33 +0200

    add default color values

--- src/Rendering/Color.h
+++ src/Rendering/Color.h
@@ -10,17 +10,28 @@ struct Color {
 
 using Pixel = std::uint32_t;
 
-[[nodiscard]] inline Pixel AsPixel(Color color) noexcept {
+[[nodiscard]] constexpr inline Pixel AsPixel(Color color) noexcept {
   return static_cast<uint32_t>(color.r) << 24 |  //
          static_cast<uint32_t>(color.g) << 16 |  //
          static_cast<uint32_t>(color.b) << 8 |   //
          static_cast<uint32_t>(color.a);
 }
-[[nodiscard]] inline Color AsColor(Pixel pixel) noexcept {
+[[nodiscard]] constexpr inline Color AsColor(Pixel pixel) noexcept {
   return {static_cast<uint8_t>(pixel >> 24),           //
           static_cast<uint8_t>((pixel >> 16) & 0xFF),  //
           static_cast<uint8_t>((pixel >> 8) & 0xFF),   //
           static_cast<uint8_t>(pixel & 0xFF)};
 }
+
+namespace Colors {
+constexpr auto Black = Color{0, 0, 0};
+constexpr auto White = Color{255, 255, 255};
+constexpr auto Red = Color{255, 0, 0};
+constexpr auto Green = Color{0, 255, 0};
+constexpr auto Blue = Color{0, 0, 255};
+constexpr auto Yellow = Color{255, 255, 0};
+constexpr auto Cyan = Color{0, 255, 255};
+constexpr auto Magenta = Color{255, 0, 255};
+}  // namespace Colors
 }  // namespace Rendering
 }  // namespace Sourcehold
