04a961f7f6f892a09dc3281c96498125bd5c7fe0
Fabian Seidl <fabianseidl@fseidl.eu>
2021-04-26 18:35:24 +0200

    add more helper functions for SDL

--- src/SDL/SDLHelpers.h
+++ src/SDL/SDLHelpers.h
@@ -2,6 +2,9 @@
 
 #include <SDL.h>
 #include <memory>
+#include <cassert>
+#include "Shapes.h"
+#include "SurfaceLock.h"
 
 namespace Sourcehold {
 namespace SDL {
@@ -31,5 +34,38 @@ using SDL_Surface_SH = std::shared_ptr<SDL_Surface>;
 using SDL_Texture_SH = std::shared_ptr<SDL_Texture>;
 using SDL_Renderer_SH = std::shared_ptr<SDL_Renderer>;
 using SDL_Window_SH = std::shared_ptr<SDL_Window>;
+
+[[nodiscard]] constexpr inline int At(Vector2<int> pos, int w) {
+  return pos.x + pos.y * w;
+}
+
+template <typename T>
+[[nodiscard]] T& At(SDL_Surface* surf, Vector2<int> pos) {
+  static_assert(std::is_arithmetic_v<T>);
+  T* ptr = static_cast<T*>(surf->pixels);
+  return ptr[At(pos, surf->w)];
+}
+
+template <typename T>
+[[nodiscard]] T& AtSafe(SDL_Surface* surf, Vector2<int> pos) {
+  static_assert(std::is_arithmetic_v<T>);
+
+  assert(surf->w > pos.x);
+  assert(surf->h > pos.y);
+
+  return At<T>(surf, pos);
+}
+
+[[nodiscard]] inline SDL_Rect* ToSDLRectPtr(Rect<int> rect) {
+  static_assert(std::is_same_v<decltype(Rect<int>::x), decltype(SDL_Rect::x)>);
+  static_assert(std::is_same_v<decltype(Rect<int>::y), decltype(SDL_Rect::y)>);
+  static_assert(std::is_same_v<decltype(Rect<int>::w), decltype(SDL_Rect::w)>);
+  static_assert(std::is_same_v<decltype(Rect<int>::h), decltype(SDL_Rect::h)>);
+  static_assert(sizeof(Rect<int>) == sizeof(SDL_Rect));
+  static_assert(sizeof(Rect<int>*) == sizeof(SDL_Rect*));
+
+  return reinterpret_cast<SDL_Rect*>(&rect);
+}
+
 }  // namespace SDL
 }  // namespace Sourcehold
