74348c8b0a21ee0381d481a890e79265fc921d20
Fabian Seidl <fabianseidl@fseidl.eu>
2021-05-01 14:33:17 +0200

    remove tests which only pass in headless or debug

--- test/SDLTest.cpp
+++ test/SDLTest.cpp
@@ -4,15 +4,6 @@
 #include "SDL/SDLBackend.h"
 using namespace Sourcehold::SDL;
 
-TEST(SDLBackend, Init) {
-  ASSERT_NO_THROW(SDLBackend::Instance());
-  ASSERT_NO_THROW(SDLBackend::Renderer());
-  ASSERT_NO_THROW(SDLBackend::Window());
-
-  ASSERT_NE(SDLBackend::Renderer(), nullptr);
-  ASSERT_NE(SDLBackend::Window(), nullptr);
-}
-
 TEST(SDLHelpers, At) {
   std::array<int, 100> groundtruth;
   std::iota(std::begin(groundtruth), std::end(groundtruth), 0);
@@ -22,14 +13,6 @@ TEST(SDLHelpers, At) {
       ASSERT_EQ(At({x, y}, 10), groundtruth[At({x, y}, 10)]);
     }
   }
-}
-
-TEST(SDLHelpers, AtSafe) {
-  Vector2<int> pos = {3, 3};
-
-  auto surface = MakeSurfaceUQ(0, 2, 2, 32, SDL_PIXELFORMAT_RGBA4444);
-
-  ASSERT_DEATH(AtSafe<uint32_t>(surface.get(), pos) = 3, "");
 }
 
 TEST(SDLHelpers, ToSDLRectPtr) {
