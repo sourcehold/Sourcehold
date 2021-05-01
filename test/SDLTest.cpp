48a416c013781a2f13cfe04d26d1c0afa08e4995
Fabian Seidl <fabianseidl@fseidl.eu>
2021-05-01 10:14:33 +0200

    add output on failure flag to ctest

--- test/SDLTest.cpp
+++ test/SDLTest.cpp
@@ -5,12 +5,12 @@
 using namespace Sourcehold::SDL;
 
 TEST(SDLBackend, Init) {
-  EXPECT_NO_THROW(SDLBackend::Instance());
-  EXPECT_NO_THROW(SDLBackend::Renderer());
-  EXPECT_NO_THROW(SDLBackend::Window());
+  ASSERT_NO_THROW(SDLBackend::Instance());
+  ASSERT_NO_THROW(SDLBackend::Renderer());
+  ASSERT_NO_THROW(SDLBackend::Window());
 
-  EXPECT_NE(SDLBackend::Renderer(), nullptr);
-  EXPECT_NE(SDLBackend::Window(), nullptr);
+  ASSERT_NE(SDLBackend::Renderer(), nullptr);
+  ASSERT_NE(SDLBackend::Window(), nullptr);
 }
 
 TEST(SDLHelpers, At) {
@@ -29,7 +29,7 @@ TEST(SDLHelpers, AtSafe) {
 
   auto surface = MakeSurfaceUQ(0, 2, 2, 32, SDL_PIXELFORMAT_RGBA4444);
 
-  EXPECT_DEATH(AtSafe<uint32_t>(surface.get(), pos) = 3, "");
+  ASSERT_DEATH(AtSafe<uint32_t>(surface.get(), pos) = 3, "");
 }
 
 TEST(SDLHelpers, ToSDLRectPtr) {
